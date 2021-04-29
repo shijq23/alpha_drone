cda6ce4d2f1e4c71114335f3c975a3e2f0c2d6a9
shijq23 <shijq23@users.noreply.github.com>
2021-04-28 23:23:07 -0400

    requirements

--- face_track/__init__.py
+++ face_track/__init__.py
@@ -1,2 +1,10 @@
+import sys
+
+if sys.version_info.major < 3:
+    msg = 'Dashboard Requires Python 3.7 or greater. You are using {}'.format(sys.version)
+    raise ValueError(msg)
+
+__version__ = '1.0.0'
+
 from .mockdjitellopy import Tello, BackgroundFrameRead
 from .pid import PID
